6b9acab2bbd5f480c88aa09af4a283d8db478bd1
Brian Quirion <quirion.brian@gmail.com>
2021-02-27 12:43:55 -0500

    mail sender: set email content as html

--- src/service/mail.py
+++ src/service/mail.py
@@ -3,11 +3,11 @@ from email.mime.text import MIMEText
 import traceback
 
 def send_mail(sender, receiver):
-    message = MIMEText('<h3>Message envoyé depuis le formulaire de contact.</h3><p>waadupp</p>')
+    message = MIMEText('<h3>Message envoyé depuis le formulaire de contact.</h3><p>waadupp</p>', 'html')
     message['Subject'] = 'Message envoyé depuis le serveur.'
     message['From'] = sender
     message['To'] = receiver
-    message['Content-Type'] = 'text/html;charset=utf-8'
+    message['Content-Type'] = 'text/html; charset=UTF-8'
     message['X-Mailer: Python']
 
     result = ''
