413b3c77142590c1ad28517db3e9a641acd70828
Brian Quirion <quirion.brian@gmail.com>
2021-02-26 21:05:02 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+import sys
+from flask import Flask
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'hi'
+
+@app.route('/mail')
+def mail():
+    return 'WUUUT'
