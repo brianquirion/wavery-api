ec5f5406fa303f51c22f06edcca8ced816764c1c
Brian Quirion <quirion.brian@gmail.com>
2021-02-27 13:27:23 -0500

    mail sender: add arguments to mail endpoint

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 import json
 import os
 import sys
-from flask import Flask
+from flask import Flask, request, jsonify
 from src.service.mail import send_mail
 
 sys.path.insert(0, os.path.dirname(__file__))
@@ -12,8 +12,26 @@ app = Flask(__name__)
 def index():
     return 'hi!!'
 
-@app.route('/mail')
+@app.route('/mail', methods=['POST'])
 def mail():
+    if 'full_name' not in request.args 
+        or 'sender' not in request.args 
+        or 'subject' not in request.args
+        or 'message' not in request.args:
+        return jsonify(message='Missing arguments in payload', status=400, category='error')
+
     with open('src/config.json') as f:
         config = json.load(f)
-    return send_mail(config['mail_from'], config['mail_to'])
+
+    body = request.json
+    email_message = '<h3>Message envoyé depuis le formulaire de contact.</h3><br/>'
+    email_message += '<strong>Nom du destinataire : </strong>{}<br/>'.format(body['full_name'])
+    email_message += '<strong>Courriel : </strong>{}<br/>'.format(body['sender'])
+    email_message += '<strong>Sujet : {}</strong><br/>'.format(body['subject'])
+    email_message += '<strong>Message : {}</strong><br/><br/>'.format(body['message'])
+
+    try:
+        send_mail(config['mail_from'], config['mail_to'], body['sender'], subject, body)
+    except Exception as e:
+        return jsonify(message=e.args[1], status=400, category='error')
+    return jsonify(status=200, category='success')
--- src/service/mail.py
+++ src/service/mail.py
@@ -2,22 +2,15 @@ from smtplib import SMTP
 from email.mime.text import MIMEText
 import traceback
 
-def send_mail(sender, receiver):
-    message = MIMEText('<h3>Message envoyé depuis le formulaire de contact.</h3><p>waadupp</p>', 'html')
-    message['Subject'] = 'Message envoyé depuis le serveur.'
+def send_mail(sender: str, receiver: str, reply_to: str, subject: str, body: str):
+    message = MIMEText(body, 'html')
+    message['Subject'] = subject
     message['From'] = sender
     message['To'] = receiver
+    message['Reply-To'] = reply_to
     message['Content-Type'] = 'text/html; charset=UTF-8'
     message['X-Mailer: Python']
 
-    result = ''
-    smtp = None
-    try:
-        smtp = SMTP('localhost')
-        smtp.sendmail(sender, receiver, message.as_string())         
-        result = 'Successfully sent email'
-    except Exception as error:
-        result = error.args[1]
-    if smtp is not None:
-        smtp.quit()
-    return result
+    smtp = SMTP('localhost')
+    smtp.sendmail(sender, receiver, message.as_string())
+    smtp.quit()
